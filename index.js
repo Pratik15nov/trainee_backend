4c1bb8033f5df3455061943b2a2c99b59520df69
FEAvansh <108047738+FEAvansh@users.noreply.github.com>
2022-07-20 10:27:13 +0530

    {changes}

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
-require("dotenv").config();
 
 
 const express = require("express");
 const mongoose = require("mongoose");
+require("dotenv").config();
 const routes = require("./routes/routes");
-const CONFIG = require('./config/config');
-const mongoString =  'mongodb+srv://'+ CONFIG.mogno.MONGO_USERNAME +':'+ CONFIG.mogno.MONGO_PASSWORD +'@' + CONFIG.mogno.MONGO_HOST + '/' + CONFIG.mogno.MONGO_DBNAME;//process.env.DATABASE_URL;
-const port = process.env.PORT || 3000
+const CONFIG = require("./config/config");
+const port = process.env.PORT || 3000;
 
-mongoose.connect(mongoString, { useNewUrlParser: true, useUnifiedTopology: true });
+const mongoString =
+  "mongodb+srv://" +
+  CONFIG.mogno.MONGO_USERNAME +
+  ":" +
+  CONFIG.mogno.MONGO_PASSWORD +
+  "@" +
+  CONFIG.mogno.MONGO_HOST +
+  "/" +
+  CONFIG.mogno.MONGO_DBNAME; //process.env.DATABASE_URL;
+
+
+mongoose.connect(mongoString, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 const database = mongoose.connection;
 
 database.on("error", (error) => {
@@ -23,7 +36,6 @@ const app = express();
 app.use(express.json());
 
 routes.initialize(app);
-
 
 app.listen(port, () => {
   console.log(`Server Started at ${port}`);
