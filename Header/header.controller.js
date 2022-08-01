01c4b34f1231d8ab51edb064e99239bd8e581b1b
FEAvansh <108047738+FEAvansh@users.noreply.github.com>
2022-08-01 18:10:19 +0530

    CHANGES

--- Header/header.controller.js
+++ Header/header.controller.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const headerService = require("../../Services/Header/header.service");
+const headerService = require("../api/Services/Header/header.service");
 const multer = require("multer");
 
 const storage = multer.diskStorage({
