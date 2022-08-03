1cba3efd8a4e08e22d9140730ca064416478ab92
FEAvansh <vansh.frontendarmy@gmail.com>
2022-08-03 15:35:12 +0530

    added stripe

--- api/Controller/Stripe/stripe.controller.js
+++ api/Controller/Stripe/stripe.controller.js
@@ -5,24 +5,28 @@ const stripe = require("stripe")(
   "sk_test_51LSJbrSHKwWbek0R6esxrRKpNKGamIvumCb53pCxkbJ69EqQ7ZgLDYgIZm6FTsI8qggGgUaPyTWrn5OB83SBuSJU009XnqRQWT"
 );
 
-router.post("/", async (req, res) => {
-  try {
-    const paymentIntent = await stripe.paymentIntents.create({
-      amount: req.body.amount,
-      currency: "inr",
-      payment_method_types: ["card"],
-    });
-    if (paymentIntent) {
-      res
-        .status(200)
-        .json({ message: "Payment successfull", data: paymentIntent });
-    } else {
-      res.status(400).json({ message: "Payment not done", data: null });
-    }
-  } catch (error) {
-    console.error(error);
-    res.status(400).json({ message: error });
-  }
+const calculateOrderAmount = (items) => {
+  // Replace this constant with a calculation of the order's amount
+  // Calculate the order total on the server to prevent
+  // people from directly manipulating the amount on the client
+  return 1400;
+};
+
+router.post("/create-payment-intent", async (req, res) => {
+  const { items } = req.body;
+
+  // Create a PaymentIntent with the order amount and currency
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: calculateOrderAmount(items),
+    currency: "inr",
+    automatic_payment_methods: {
+      enabled: true,
+    },
+  });
+
+  res.send({
+    clientSecret: paymentIntent.client_secret,
+  });
 });
 
 module.exports = router;
