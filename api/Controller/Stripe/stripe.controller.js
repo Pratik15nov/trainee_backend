cb5fb229774b6e7330d27c93bd4804f2e7751023
FEAvansh <vansh.frontendarmy@gmail.com>
2022-08-03 15:59:14 +0530

    added stripe

--- api/Controller/Stripe/stripe.controller.js
+++ api/Controller/Stripe/stripe.controller.js
@@ -5,28 +5,30 @@ const stripe = require("stripe")(
   "sk_test_51LSJbrSHKwWbek0R6esxrRKpNKGamIvumCb53pCxkbJ69EqQ7ZgLDYgIZm6FTsI8qggGgUaPyTWrn5OB83SBuSJU009XnqRQWT"
 );
 
-const calculateOrderAmount = (items) => {
+const calculateOrderAmount = () => {
   // Replace this constant with a calculation of the order's amount
   // Calculate the order total on the server to prevent
   // people from directly manipulating the amount on the client
   return 1400;
 };
 
-router.post("/create-payment-intent", async (req, res) => {
-  const { items } = req.body;
-
+router.get("/create-payment-intent", async (req, res) => {
   // Create a PaymentIntent with the order amount and currency
   const paymentIntent = await stripe.paymentIntents.create({
-    amount: calculateOrderAmount(items),
+    amount: calculateOrderAmount(),
     currency: "inr",
     automatic_payment_methods: {
       enabled: true,
     },
   });
-
-  res.send({
-    clientSecret: paymentIntent.client_secret,
-  });
+  res.status(200).json({
+    success: true,
+    message: 'Client Secret Key',
+    data: paymentIntent.client_secret
+  })
+  // res.send({
+  //   clientSecret: paymentIntent.client_secret,
+  // });
 });
 
 module.exports = router;
