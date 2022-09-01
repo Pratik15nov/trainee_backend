f87000a38742d5e8a9b56360e09c195733fad659
FEAvansh <vansh.frontendarmy@gmail.com>
2022-09-01 13:01:29 +0530

    added search Order

--- api/Controller/Order/order.controller.js
+++ api/Controller/Order/order.controller.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const OrderService = require("../../Services/Order/order.service");
+const OrderModal = require("../../Services/Order/order.modal");
 
 router.post("/", async (req, res) => {
   try {
@@ -43,6 +44,28 @@ router.post("/list", async (req, res) => {
       return res.status(200).json({ success, message, data });
     } else {
       return res.status(400).json({ success, message, data });
+    }
+  } catch (error) {
+    res.status(400).json({ message: error });
+  }
+});
+router.post("/search", async (req, res) => {
+  try {
+    let searchText = req.body.searchText;
+    const result = await OrderModal.find({
+      $or: [{ paymentId: { $regex: ".*" + searchText + ".*", $options: "i" } }],
+    });
+    console.log("result: ", result);
+    if (result.length > 0) {
+      return res.status(200).json({
+        success: true,
+        message: "data found successfully",
+        data: result,
+      });
+    } else {
+      return res
+        .status(400)
+        .json({ success: false, message: "data  not found", data: [] });
     }
   } catch (error) {
     res.status(400).json({ message: error });
