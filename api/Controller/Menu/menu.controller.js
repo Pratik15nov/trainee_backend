3d274c73e762f38a5b041a7d29bb1a0c3be52829
FEAvansh <vansh.frontendarmy@gmail.com>
2022-09-12 17:53:49 +0530

    "updated data"

--- api/Controller/Menu/menu.controller.js
+++ api/Controller/Menu/menu.controller.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const MenuService = require("../../Services/Menu/menu.service");
+const menuModal = require("../../Services/Menu/menu.modal");
 
 router.post("/", async (req, res) => {
   try {
@@ -57,6 +58,29 @@ router.post("/list", async (req, res) => {
       return res.status(200).json({ success, message, data });
     } else {
       return res.status(400).json({ success, message, data });
+    }
+  } catch (error) {
+    res.status(400).json({ message: error });
+  }
+});
+
+router.post("/search", async (req, res) => {
+  try {
+    let searchText = req.body.searchText;
+
+    const result = await menuModal.find({
+      fieldName: { $regex: ".*" + searchText + ".*", $options: "i" },
+    });
+    if (result.length > 0) {
+      return res.status(200).json({
+        success: true,
+        message: "data found successfully",
+        data: result,
+      });
+    } else {
+      return res
+        .status(400)
+        .json({ success: false, message: "data  not found", data: [] });
     }
   } catch (error) {
     res.status(400).json({ message: error });
